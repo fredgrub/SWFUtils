2c0d2a7155302ef073b63d4831a035bcc3b5d17d
Lucas de Sousa Rosa <roses.lucas404@gmail.com>
2023-05-18 23:11:04 -0300

    Create the workload class

---
+++ SWFUtils/workload.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+class Workload:
+    def __init__(self):
+        self.meta = {}
+        self.jobs = pd.DataFrame()
+
+    def do_something(self):
+        # TODO: Implement this method
+        pass
